568b1dbcf7a6ed86176d477c24ca6372f07ab323
Kim Cressman <kim@catbirdstats.com>
2023-12-22 11:43:23 -0600

    added several metrics

--- R/sourced/001_pre-processing.R
+++ R/sourced/001_pre-processing.R
@@ -21,9 +21,6 @@ dat <- dat %>%
     dplyr::relocate(c(Date, Years_sinceStart), .before = Year) %>% 
     dplyr::relocate(c(StTrns, StTrnsPlt), .after = PlotID)
 
-# clean up data but keep some remnants for QA/QC tables
-unsampds <- find_unsampleds(dat)
-susps <- find_suspect_values(dat, "-3")
 dat <- remove_suspect_values(dat, flags = c("-3"))  # remove suspect values. also removes F_columns.
 dat <- remove_unsampleds(dat)  # should get rid of any dates where a plot was not sampled or was entirely rejected. 
 dat <- na_to_0(dat)  # turn remaining NAs into 0s - this first makes sure all species columns are numeric
@@ -31,9 +28,29 @@ dat <- join_zones(dat, stn_tbl)  # add vegetation zones to each row
 dat_long <- dat %>%
     pivot_longer(-(Reserve:Total),
                  names_to = "Species",
-                 values_to = "Cover")
+                 values_to = "Cover") %>% 
+    left_join(species_info, by = "Species") %>% 
+    mutate(Invasive = case_when(Native_Classification %in% c("Native invasive", "Non-native invasive") ~ "Invasive",
+                                .default = "not invasive"),
+           Salt_or_Fresh = case_when(Plant_Categories %in% c("B-Brackish", "H-Halophyte", "A-Algae") ~ "Salt",
+                                     Plant_Categories %in% c("F-Freshwater", "U-Upland") ~ "Fresh",
+                                     .default = "Neither"),
+           grouping_category = case_when(!is.na(NMST_Groupings) ~ NMST_Groupings,
+                                         !is.na(Plant_Categories) ~ Plant_Categories,
+                                         .default = Cover_Categories))
 
-# calculate metrics
+
+# make the grouped data frame
+dat_grouped <- dat_long %>% 
+    summarize(.by = c(Reserve, SiteID, TransectID, PlotID, Year, Month, Day, grouping_category),
+              Cover = sum(Cover, na.rm = TRUE)) %>% 
+    mutate(grouping_category = case_when(grouping_category == "Live vegetation" ~ "Other live vegetation",
+                                         .default = grouping_category)) %>% 
+    pivot_wider(names_from = grouping_category,
+                values_from = Cover)
+
+
+# calculate metrics and add to both data frames
 spps_live <- species_info$Species[species_info$Cover_Categories == "Live vegetation"]
 spps_live <- spps_live[which(spps_live %in% names(dat))]
 dat_live <- dat %>% 
@@ -41,11 +58,12 @@ dat_live <- dat %>%
 swdiv <- diversity(dat_live, index = "shannon")
 rich <- specnumber(dat_live)
 dat_div <- dat %>% 
-    select(Reserve, StTrnsPlt, Year, Month, Day) %>% 
+    select(Reserve, SiteID, TransectID, PlotID, Year, Month, Day) %>% 
     mutate(SWdiv = swdiv,
            Richness = rich)
 dat <- left_join(dat, dat_div)
-rm(dat_live, swdiv, rich)
+dat_grouped <- left_join(dat_grouped, dat_div)
+rm(dat_live, swdiv, rich, dat_div)
 
 # attach EIR to data frame
 EIR <- dat_long %>% 
@@ -56,25 +74,19 @@ EIR <- dat_long %>%
     group_by(Year, Month, Day, Reserve, SiteID, TransectID, PlotID) %>% 
     summarize(EIR = round(sum(EI_Cover) / sum(Cover), 5))
 dat <- left_join(dat, EIR)
+dat_grouped <- left_join(dat_grouped, EIR)
+rm(EIR)
 
-
-others <- dat_long %>% 
-    left_join(species_info, by = "Species") %>% 
-    mutate(Invasive = case_when(Native_Classification %in% c("Native invasive", "Non-native invasive") ~ "Invasive",
-                                .default = "not invasive"),
-           Salt_or_Fresh = case_when(Plant_Categories %in% c("B-Brackish", "H-Halophyte", "A-Algae") ~ "Salt",
-                                     Plant_Categories %in% c("F-Freshwater", "U-Upland") ~ "Fresh",
-                                     .default = "Neither"))
-
-Invasives <- others %>% 
+Invasives <- dat_long %>% 
     summarize(.by = c(Year, Month, Day, Reserve, SiteID, TransectID, PlotID, Invasive),
               Invasive_Cover = sum(Cover, na.rm = TRUE)) %>% 
     filter(Invasive == "Invasive") %>% 
     select(Year, Month, Day, Reserve, SiteID, TransectID, PlotID, Invasive_Cover)
 dat <- left_join(dat, Invasives)
+dat_grouped <- left_join(dat_grouped, Invasives)
+rm(Invasives)
 
-
-Salt_or_Fresh <- others %>% 
+Salt_or_Fresh <- dat_long %>% 
     summarize(.by = c(Year, Month, Day, Reserve, SiteID, TransectID, PlotID, Salt_or_Fresh),
               Cover = sum(Cover, na.rm = TRUE)) %>% 
     pivot_wider(names_from = Salt_or_Fresh,
@@ -82,9 +94,10 @@ Salt_or_Fresh <- others %>%
     mutate(Salt_to_Fresh = Salt / Fresh) %>% 
     select(Year, Month, Day, Reserve, SiteID, TransectID, PlotID, Salt_to_Fresh)
 dat <- left_join(dat, Salt_or_Fresh)
+dat_grouped <- left_join(dat_grouped, Salt_or_Fresh)
+rm(Salt_or_Fresh)
 
-
-Unveg_to_veg <- others %>% 
+Unveg_to_veg <- dat_long %>% 
     summarize(.by = c(Year, Month, Day, Reserve, SiteID, TransectID, PlotID, Cover_Categories),
               Cover = sum(Cover, na.rm = TRUE)) %>% 
     pivot_wider(names_from = Cover_Categories,
@@ -92,3 +105,21 @@ Unveg_to_veg <- others %>%
     mutate(Unveg_to_veg = `Unvegetated category` / `Live vegetation`) %>% 
     select(Year, Month, Day, Reserve, SiteID, TransectID, PlotID, Unveg_to_veg)
 dat <- left_join(dat, Unveg_to_veg)
+dat_grouped <- left_join(dat_grouped, Unveg_to_veg)
+rm(Unveg_to_veg)
+
+
+total_live_abiotic <- dat_long %>% 
+    summarize(.by = c(Year, Month, Day, Reserve, SiteID, TransectID, PlotID, Cover_Categories),
+              Cover = sum(Cover, na.rm = TRUE)) %>% 
+    mutate(Cover_Categories = case_when(Cover_Categories == "Unvegetated category" ~ "Total unvegetated",
+                                        Cover_Categories == "Live vegetation" ~ "Total live veg",
+                                        .default = Cover_Categories)) %>% 
+    pivot_wider(names_from = Cover_Categories,
+                values_from = Cover) %>% 
+    select(Year, Month, Day, Reserve, SiteID, TransectID, PlotID,
+           `Total unvegetated`,
+           `Total live veg`)
+dat <- left_join(dat, total_live_abiotic)
+dat_grouped <- left_join(dat_grouped, total_live_abiotic)
+rm(total_live_abiotic)
